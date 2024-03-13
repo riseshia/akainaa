61d09d9d36f3ba2a1e2fc87e6489e8317c7845fe
Shia <rise.shia@gmail.com>
2024-03-13 22:11:26 +0900

    Update view

--- lib/isucover.rb
+++ lib/isucover.rb
@@ -23,6 +23,10 @@ module Isucover
         .select { |k, _v| k.start_with?(project_dir) }
         .transform_keys { |k| k.sub(project_dir, '') }
     end
+
+    def reset
+      Coverage.result(stop: false, clear: true)
+    end
   end
 
   class Middleware
@@ -34,19 +38,73 @@ module Isucover
 
     def call(env)
       if env['PATH_INFO'] == '/isucover'
-        matched = env['QUERY_STRING'].match(/path=([^&]+)/)
-        path = matched ? matched[1] : 'app.rb'
+        path = extract_path_from_query(env)
+        html = render_page(path)
 
-        result = Isucover.peek_result[path]
-        html = render_result(path, result)
         [200, { 'Content-Type' => 'text/html;charset=utf-8' }, [html]]
+      elsif env['PATH_INFO'] == '/isucover/reset'
+        path = extract_path_from_query(env)
+        Isucover.reset
+
+        [302, { 'Location' => "/isucover?path=#{path}" }, [html]]
       else
         @app.call(env)
       end
     end
 
-    private def render_result(path, result)
-      if result.nil?
+    private def extract_path_from_query(env)
+      matched = env['QUERY_STRING'].match(/path=([^&]+)/)
+      matched ? matched[1] : 'app.rb'
+    end
+
+    private def render_line(lineno, code, count, count_top)
+      <<~HTML
+        <div class="line pure-g count-p#{count_top}">
+          <div class="executed-count pure-u-2-24">
+            <p>#{count}</p>
+          </div>
+          <div class="lineno pure-u-2-24">
+            <p>#{lineno}:</p>
+          </div>
+          <div class="code pure-u-20-24">
+            <pre class="code language-ruby">#{code}</pre>
+            </code>
+          </div>
+        </div>
+      HTML
+    end
+
+    private def render_filelist(files, current_path:)
+      li_elements = files.sort.map do |file|
+        class_suffix = file == current_path ? ' current' : ''
+        <<~HTML
+          <li class="pure-menu-item">
+            <a href="/isucover?path=#{file}" class="pure-menu-link filepath#{class_suffix}">#{file}</a>
+          </li>
+        HTML
+      end.join
+
+      <<~HTML
+        <div>
+          <div class="pure-menu">
+            <span class="pure-menu-heading">Isucover</span>
+            <ul class="pure-menu-list">
+              <li class="pure-menu-item">
+                <a href="/isucover/reset" class="pure-button pure-button-primary">Reset</a>
+              </li>
+              <li class="pure-menu-heading">Files</li>
+              #{li_elements}
+            </ul>
+          </div>
+        </div>
+      HTML
+    end
+
+    private def render_page(path)
+      result = Isucover.peek_result
+      path_result = result[path]
+
+      if path_result.nil?
         return "There is emtpy result for #{path}"
       end
 
@@ -54,41 +112,116 @@ module Isucover
         return "<" + "p>#{path} not found.<" + "/p>"
       end
 
-      coverage_on_line = result[:lines]
+      filelist = render_filelist(result.keys, current_path: path)
 
-      meta_infos = []
-      codes = []
+      coverage_on_line = path_result[:lines]
+      max_count = coverage_on_line.max_by(&:to_i).to_i + 1
+
+      lines = []
       File.read(path).each_line.with_index do |line, index|
-        prefix = coverage_on_line[index].to_s.rjust(PADDING)
-        lineno = (index + 1).to_s.rjust(PADDING)
+        count = coverage_on_line[index].to_i
+        count_top = (count * 10 / max_count).to_i * 10
+        line = render_line(index + 1, line, coverage_on_line[index], count_top)
 
-        meta_infos << "#{prefix} #{lineno}:"
-        codes << line
+        lines << line
       end
 
       <<~HTML
+        <!DOCTYPE html>
         <html>
-        <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/ruby.min.js"></script>
-        <style>
-        </style>
-        </head>
-        <body>
-        <table>
-        <tr>
-        <td>
-        <pre>#{meta_infos.join("\n")}</pre>
-        </td>
-        <td>
-        <pre><code class="language-ruby">#{codes.join}</code></pre>
-        </td>
-        </tr>
-        </table>
+          <head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/a11y-light.min.css">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/ruby.min.js"></script>
+            <style>
+              .sidebar {
+                background-color: #eee;
+              }
 
-        <script>hljs.highlightAll();</script>
-        </body>
+              .line {
+                line-height: 20px;
+              }
+
+              .executed-count, .lineno {
+                text-align: right;
+              }
+
+              p {
+                padding: 0 5 0 5;
+                margin: 0px;
+              }
+
+              pre.code {
+                margin: 0px;
+                background: transparent;
+              }
+
+              a.filepath {
+                text-wrap: balance;
+                word-break: break-all;
+              }
+              a.filepath.current {
+                font-weight: bold;
+              }
+
+              div.count-p90 {
+                background-color: #ff392e;
+              }
+              div.count-p90:hover {
+                background-color: #fedcda;
+              }
+              div.count-p80 {
+                background-color: #ff5047;
+              }
+              div.count-p80:hover {
+                background-color: #fedcda;
+              }
+              div.count-p70 {
+                background-color: #ff675f;
+              }
+              div.count-p70:hover {
+                background-color: #fedcda;
+              }
+              div.count-p60 {
+                background-color: #ff7f78;
+              }
+              div.count-p50 {
+                background-color: #ff9690;
+              }
+              div.count-p40 {
+                background-color: #ffada9;
+              }
+              div.count-p30 {
+                background-color: #fec4c1;
+              }
+              div.count-p20 {
+                background-color: #fedcda;
+              }
+              div.count-p10 {
+                background-color: #ffffff;
+              }
+              div.count-p00 {
+                background-color: #a3cfbb;
+              }
+            </style>
+          </head>
+          <body>
+            <div id="layout" class="pure-g">
+              <div class="sidebar pure-u-1-4">
+                #{filelist}
+              </div>
+              <div class="content pure-u-3-4">
+                #{lines.join("\n")}
+              </div>
+            </div>
+            <script>
+              document.querySelectorAll('pre.code').forEach(el => {
+                hljs.highlightElement(el);
+              });
+            </script>
+          </body>
         </html>
       HTML
     end
